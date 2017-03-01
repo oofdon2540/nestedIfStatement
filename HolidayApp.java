2c73c017955a83eb0fceb945a575fde93823b2cf
oofdon2540 <komsan_58@hotmail.com>
2017-03-01 14:33:43 +0000

    loop

--- HolidayApp.java
+++ HolidayApp.java
@@ -11,12 +11,13 @@ public class HolidayApp{
 		int cost;
 		int people;
 		String season;
-
+		String answer;
 		//objects
 		Holiday myHoliday;
 		myHoliday =new Holiday();
 
 		//inputs
+		do{
 		season=JOptionPane.showInputDialog(null, "What season is it?");
 		myHoliday.setSeason(season);
 
@@ -29,5 +30,7 @@ public class HolidayApp{
 		//output
 		cost=myHoliday.getCost();
 		JOptionPane.showMessageDialog(null, "Your journey cost " +cost);
+		answer=JOptionPane.showInputDialog(null, "Again?");
+		}while(answer.equalsIgnoreCase("y"));
 }
 }
